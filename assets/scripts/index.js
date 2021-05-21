a5181b4376c67705a2457f8120426f83f78c728e
Laura Little <laura.nathanson@gmail.com>
2021-05-20 21:36:08 -0400

    added T-Mobile license disclaimer to code file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2021 T-Mobile USA, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * See the LICENSE.txt file for additional language around the disclaimer of warranties.
+ * Trademark Disclaimer: Neither the name of “T-Mobile, USA” nor the names of
+ * its contributors may be used to endorse or promote products
+ *
+ */
 // Allow :active styles to work in your CSS on a page in Mobile Safari:
 document.addEventListener("touchstart", function(){}, true);
 
