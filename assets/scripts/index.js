f85312e04ab0c7597a9afea917993ff8dcd462d7
charlietriplett <charlie@charliecharliecharlie.com>
2021-07-22 09:25:33 -0700

    Troubleshooting iOS VO

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -34,7 +34,7 @@ $("#show-password").on('change', function() {
 });
 
 
-$(".interactive").mouseup(function() {
+$(".interactive").click(function() {
     if ($(this).find('input[type="checkbox"]').is(':checked') && !$(this).find('input[type="checkbox"]').is(':focus') ) {
         $(this).find('input[type="checkbox"]').prop('checked', false);
         $(this).removeClass('checked');
