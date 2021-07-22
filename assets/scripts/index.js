5aa01fdd107c4789ece12dc9cc71c0a4ecbf9ae1
charlietriplett <charlie@charliecharliecharlie.com>
2021-07-22 08:20:36 -0700

    Swapped out div for <ul> <li>

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -34,7 +34,7 @@ $("#show-password").on('change', function() {
 });
 
 
-$(".interactive").mousedown(function() {
+$(".interactive").mouseup(function() {
     if ($(this).find('input[type="checkbox"]').is(':checked') ) {
         $(this).find('input[type="checkbox"]').prop('checked', false);
         $(this).removeClass('checked');
