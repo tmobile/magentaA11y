d50568f92aa37273f37b8eb9014fa859d276e9c7
silverli <lindsaysilver1@gmail.com>
2025-06-17 16:47:19 -0400

    fix: add copyright year to footer/ header css adjustments

--- src/components/navigation/footer/footer.tsx
+++ src/components/navigation/footer/footer.tsx
@@ -8,39 +8,44 @@ import { DocumentationCategory } from 'shared/types/shared-types';
 
 import './footer.scss';
 
+const currentYear = new Date();
+
 const Footer: React.FC = () => {
   return (
-    <footer tabIndex={-1} className="MagentaA11y--footer">
-      {Object.entries(contentData).map(([category, items]) => (
-        <div key={category} className="MagentaA11y--footer__nav-list">
-          <h2>
-            {category
-              .replace(/-/g, ' ')
-              .replace(/\b\w/g, (c) => c.toUpperCase())}{' '}
-            {category === DocumentationCategory.HOW_TO_TEST ? '' : 'Criteria'}
-          </h2>
+    <footer tabIndex={-1}>
+      <div className="MagentaA11y--footer">
+        {Object.entries(contentData).map(([category, items]) => (
+          <div key={category} className="MagentaA11y--footer__nav-list">
+            <h2>
+              {category
+                .replace(/-/g, ' ')
+                .replace(/\b\w/g, (c) => c.toUpperCase())}{' '}
+              {category === DocumentationCategory.HOW_TO_TEST ? '' : 'Criteria'}
+            </h2>
+            <Divider orientation={OrientationEnum.HORIZONTAL} />
+            <ul role="list">
+              {items.map((item) => (
+                <li key={item.name} role="listitem">
+                  <NavLink to={`${category}-criteria/${item.name}/overview`}>
+                    {item.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        {/* TODO - also unskip test in footer.test.tsx */}
+        {/* <div className="MagentaA11y--footer__nav-list">
+          <h2>About Us</h2>
           <Divider orientation={OrientationEnum.HORIZONTAL} />
           <ul role="list">
-            {items.map((item) => (
-              <li key={item.name} role="listitem">
-                <NavLink to={`${category}-criteria/${item.name}/overview`}>
-                  {item.label}
-                </NavLink>
-              </li>
-            ))}
+            <li role="listitem">
+              <NavLink to={`/about`}>Contact Us</NavLink>
+            </li>
           </ul>
-        </div>
-      ))}
-      {/* TODO - also unskip test in footer.test.tsx */}
-      {/* <div className="MagentaA11y--footer__nav-list">
-        <h2>About Us</h2>
-        <Divider orientation={OrientationEnum.HORIZONTAL} />
-        <ul role="list">
-          <li role="listitem">
-            <NavLink to={`/about`}>Contact Us</NavLink>
-          </li>
-        </ul>
-      </div> */}
+        </div> */}
+      </div>
+      <p className='date'>&copy; {currentYear.getFullYear()} T-Mobile USA, Inc.</p>
     </footer>
   );
 };
