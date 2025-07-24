3aa8f88a432ae7778fb5622f76f2787cabcdbcef
Lawrence Moore <thelawrencemoore@gmail.com>
2025-07-24 15:42:59 -0700

    Creatd hero section

--- src/components/about-us/about-us.tsx
+++ src/components/about-us/about-us.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import "../../about.scss";
 
 const AboutUs: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to MagentaA11y</h1>
-      <p>This is the About Us page.</p>
+    <div className="MagentaA11y--hero-container-wrapper">
+      <div className="MagentaA11y--hero-container">
+        <h1 className="MagentaA11y--home-page__header--title MagentaA11y--color">
+          We Are <span>MagentaA11y</span>
+        </h1>
+        <p>Some catchy tagline for ARC</p>
+
+        <img src="https://placehold.co/800x400" alt="placeholder" />
+
+        <p>
+          The <b>Accessibility Resource Center at T&#8209;Mobile</b> brings
+          universal design and accessibility (a11y) to all aspects of
+          T&#8209;Mobile digital products.
+        </p>
+      </div>
     </div>
   );
 };
